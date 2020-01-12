e11a21edfb07f04addd63157344d92d921d1a49f
Yojan Regmi <devyojan@gmail.com>
2020-01-12 21:47:04 +0400

    Controlled search input

---
+++ src/components/Searchbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    //TODO: Call callback from parent component
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" name="video" value={this.state.input} onChange={this.handleChange} autoComplete="off" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
